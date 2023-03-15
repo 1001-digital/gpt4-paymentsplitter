affce342c8ad24a533827eb4f5de083f7156f7de
jalil <jalil@1001.digital>
2023-03-15 11:41:31 +0100

    Configure for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,53 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import { config as dotenvConfig } from 'dotenv'
+import "@nomicfoundation/hardhat-toolbox"
+import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+
+dotenvConfig()
+
+const {
+  COINMARKETCAP_API_KEY,
+  ETHERSCAN_API_KEY,
+  GOERLI_URL ,
+  MAINNET_URL,
+  PRIVATE_KEY,
+  REPORT_GAS,
+} = process.env
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: GOERLI_URL || "",
+      accounts:
+        PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: MAINNET_URL || "",
+      accounts:
+        PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+    // Add more networks here if needed, e.g., testnets or mainnets
+  },
+  gasReporter: {
+    enabled: REPORT_GAS !== undefined,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    currency: "USD",
+    // gasPrice: 20,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+}
 
-export default config;
+export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,23 @@
-import { ethers } from "hardhat";
+// scripts/deploy.js
+const hre = require('hardhat')
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await hre.ethers.getSigners()
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  console.log('Deploying contracts with the account:', deployer.address)
+  console.log('Account balance:', (await deployer.getBalance()).toString())
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const PaymentSplitterFactory = await hre.ethers.getContractFactory('PaymentSplitter')
+  const PaymentSplitter = await PaymentSplitterFactory.deploy()
 
-  await lock.deployed();
+  await PaymentSplitter.deployed()
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log('PaymentSplitter deployed to:', PaymentSplitter.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
